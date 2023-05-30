32c88b7f35763fb81e9c835060c1540bf0cf01a3
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-30 19:47:26 +0530

    update homeFunction.js

--- javascript/homeFunction.js
+++ javascript/homeFunction.js
@@ -2,4 +2,9 @@ window.onload = () =>{
 
     let facebook_currentUser = JSON.parse(localStorage.getItem("facebook_current_user"));
 
+    if(!!facebook_currentUser){
+
+    } else {
+        window.location.href = `./login.html`;
+    }
 }
