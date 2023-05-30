8b9c33f4308112ad9fa2d35573e8027c42e1df46
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-30 19:46:31 +0530

    commit

---
+++ javascript/homeFunction.js
@@ -0,0 +1,5 @@
+window.onload = () =>{
+
+    let facebook_currentUser = JSON.parse(localStorage.getItem("facebook_current_user"));
+
+}
--- javascript/userFunction.js
+++ javascript/userFunction.js
@@ -16,18 +16,14 @@ function facebook_register(event){
     if(f_userName && f_userEmail && f_userPassword && f_userConfirmPassword && f_userDateDay && f_userDateMonth && f_userDateYear){
         if(f_userPassword.length >=8 && f_userConfirmPassword>=8){
             if(f_userPassword === f_userConfirmPassword){
-
                 let facebook_userData = JSON.parse(localStorage.getItem("facebook_users")) || [];
                 let flag = false;
-
                 for(let i=0;i<facebook_userData.length;i++){
                     if(f_userEmail == facebook_userData[i].email){
                         flag = true;
                     }
                 }
-
                 if(!flag){
-
                     const dateOfBirth = new Date(f_userDateYear, f_userDateMonth-1, f_userDateDay);
                     let facebook_object = {
                         username: f_userName,
@@ -49,14 +45,10 @@ function facebook_register(event){
                     document.getElementById("facebook_day").value = "";
                     document.getElementById("facebook_month").value = "";
                     document.getElementById("facebook_year").value = "";
-
-
                 } else {
                     alert("Email is already registered. Proceed To Login.");
                     window.location.href = `./home.html`;
                 }
-
-
             }
         } else {
             alert("Password should be more than 8 characters.");
@@ -64,9 +56,49 @@ function facebook_register(event){
     } else {
         alert("All fields are required.");
     }
+}
 
 
+function facebook_login(event){
+    event.preventDefault();
     
+    let f_loginUserEmail = document.getElementById("facebook_login_email").value;
+    let f_loginPassword = document.getElementById("facebook_login_password").value;
+
+    if(f_loginUserEmail && f_loginPassword){
+
+        let flag=false;;
+        let facebook_userData = JSON.parse(localStorage.getItem("facebook_users"));
+        let facebook_current_user;
+
+        for(let i=0;i<facebook_userData.length;i++){
+            if(f_loginUserEmail === facebook_userData[i].email && f_loginPassword === facebook_userData[i].password){
+                flag = true;
+                facebook_current_user = facebook_userData[i];
+            }
+        }
+
+        if(flag){
+            localStorage.setItem("facebook_current_user", JSON.stringify(facebook_current_user));
+            alert("Login successful.");
+            window.location.href = `./home.html`;
+
+        } else {
+            alert("Please check your credentials.");
+        }
+
+    } else {
+        alert("All fields are mandatory.");
+    }
+
+}
 
 
+function redirectToRegister(){
+    window.location.href = `./register.html`;
+}
+
+
+function redirectToLogin(){
+    window.location.href = `./login.html`;
 }
